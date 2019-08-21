e0923e4a6e0fb1fcec6ea95921b543da57e11809
comphonia <online.comphonia@gmail.com>
2019-08-21 12:09:21 -0500

    Application completed, pre-deployment

--- src/App.js
+++ src/App.js
@@ -1,26 +1,126 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Router, navigate } from "@reach/router";
+import firebase from "./firebase/Firebase";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from "./components/Home";
+import Welcome from "./components/Welcome";
+import Navigation from "./components/Navigation";
+import Login from "./components/Login";
+import Meetings from "./components/Meetings";
+import Register from "./components/Register";
+import CheckIn from "./components/CheckIn";
+import Attendees from "./components/Attendees";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: null,
+      displayName: null,
+      uid: null
+    };
+  }
+
+  componentDidMount() {
+    /* const ref = firebase.database().ref("user");
+    ref.on("value", snapshot => {
+      let FBUser = snapshot.val();
+      this.setState({ user: FBUser });
+    }); */
+
+    firebase.auth().onAuthStateChanged(FBUser => {
+      if (FBUser) {
+        this.setState({
+          user: FBUser,
+          displayName: FBUser.displayName,
+          userID: FBUser.uid
+        });
+
+        const meetingRef = firebase.database().ref("meetings/" + FBUser.uid);
+
+        meetingRef.on("value", snapshot => {
+          let meetings = snapshot.val();
+          let meetingsList = [];
+
+          for (let item in meetings) {
+            meetingsList.push({
+              meetingID: item,
+              meetingName: meetings[item].meetingName
+            });
+          }
+          this.setState({
+            meetings: meetingsList,
+            noOfMeetings: meetingsList.length
+          });
+        });
+      } else {
+        this.setState({ user: null });
+      }
+    });
+  }
+
+  registerUser = userName => {
+    firebase.auth().onAuthStateChanged(FBUser => {
+      FBUser.updateProfile({ displayName: userName }).then(() => {
+        this.setState({
+          user: FBUser,
+          displayName: FBUser.displayName,
+          userID: FBUser.uid
+        });
+        navigate("/meetings");
+      });
+    });
+  };
+
+  logOutUser = e => {
+    e.preventDefault();
+    this.setState({
+      displayName: null,
+      userID: null,
+      user: null
+    });
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        navigate("/login");
+      });
+  };
+
+  addMeeting = meetingName => {
+    const refUser = firebase.database().ref(`meetings/${this.state.user.uid}`);
+    refUser.push({ meetingName });
+  };
+
+  render() {
+    return (
+      <div>
+        <Navigation user={this.state.user} logOutUser={this.logOutUser} />
+        {this.state.user && (
+          <Welcome
+            userName={this.state.displayName}
+            logOutUser={this.logOutUser}
+          />
+        )}
+        <Router>
+          <Home path="/" user={this.state.user} />
+          <Login path="/login" />
+          <Meetings
+            path="/meetings"
+            meetings={this.state.meetings}
+            addMeeting={this.addMeeting}
+            userID={this.state.userID}
+          />
+          <Attendees
+            path="/attendees/:userID/:meetingID"
+            adminUser={this.state.userID}
+          />
+          <CheckIn path="/checkin/:userID/:meetingID" />
+          <Register path="/register" registerUser={this.registerUser} />
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
